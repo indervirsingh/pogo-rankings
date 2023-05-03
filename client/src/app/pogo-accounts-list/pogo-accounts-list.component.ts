78be08aaf0ed66ad768af22b3e801a362b8649de
Indervir Singh <iindervirsingh@gmail.com>
2023-05-03 15:54:01 -0700

    generated pogo-accounts-list component

---
+++ client/src/app/pogo-accounts-list/pogo-accounts-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pogo-accounts-list',
+  template: `
+    <p>
+      pogo-accounts-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PogoAccountsListComponent {
+
+}
