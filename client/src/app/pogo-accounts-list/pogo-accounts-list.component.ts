1c363e9a4cd20d0478b99f950f03926f4f4a7f0e
Indervir Singh <iindervirsingh@gmail.com>
2023-05-03 16:12:46 -0700

    updating component UI

--- client/src/app/pogo-accounts-list/pogo-accounts-list.component.ts
+++ client/src/app/pogo-accounts-list/pogo-accounts-list.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { PogoAccounts } from '../pogo-accounts'
+import { PogoAccountsService } from '../pogo-accounts.service'
+
+// Need to fix module/folder import
 
 @Component({
   selector: 'app-pogo-accounts-list',
   template: `
-    <p>
-      pogo-accounts-list works!
-    </p>
+    <h2 class="text-center m-5">Accounts List</h2>
+    
+    <table class="table table-striped table-bordered">
+      <thead class="thead-dark">
+        <tr>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Team</th>
+          <th>Country</th>
+          <th>Birthday</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr *ngFor="let pogoAccount of pogoAccounts$ | async">
+          <td>{{ pogoAccount.username }}</td>
+          <td>{{ pogoAccount.email }}</td>
+          <td>{{ pogoAccount.team }}</td>
+          <td>{{ pogoAccount.country }}</td>
+          <td>{{ pogoAccount.birthday }}</td>
+              <button class="btn btn-primary me-1" [routerLink]="['edit/', pogoAccount._id]">Edit</button>
+              <button class="btn btn-danger" (click)="deletePogoAccount(pogoAccount._id || '')">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <button class="btn btn-primary" [routerLink]="['new']">Add a New Account</button>
+
   `,
   styles: [
   ]
 })
+
 export class PogoAccountsListComponent {
 
 }
