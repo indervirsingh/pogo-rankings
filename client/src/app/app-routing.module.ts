923be4f43a7dd7161cc08f67a48113e5f33eb755
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 14:54:53 -0700

    importing routes via pogo-accounts-list-component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PogoAccountsListComponent } from './pogo-accounts-list/pogo-accounts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pogo-accounts', pathMatch: 'full' },
+  { path: 'pogo-accounts', component: PogoAccountsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
