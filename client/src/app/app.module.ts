d188d1341c2e838f774602bbc9021a3d2de10835
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 15:03:03 -0700

    generated form component to add new accounts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PogoAccountsListComponent } from './pogo-accounts-list/pogo-accounts-list.component';
+import { PogoAccountsFormComponent } from '../pogo-accounts-form/pogo-accounts-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PogoAccountsListComponent
+    PogoAccountsListComponent,
+    PogoAccountsFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/pogo-accounts-form/pogo-accounts-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pogo-accounts-form',
+  template: `
+    <p>
+      pogo-accounts-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PogoAccountsFormComponent {
+
+}
