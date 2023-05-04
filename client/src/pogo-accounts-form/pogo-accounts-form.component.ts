3e6a06bf00196d9d615aa5c8231cede33ad35538
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 16:19:09 -0700

    refactoring email and team sections

--- client/src/pogo-accounts-form/pogo-accounts-form.component.ts
+++ client/src/pogo-accounts-form/pogo-accounts-form.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
     <form class="pogo-accounts-form" autocomplete="off" [formGroup]="pogoAccountsForm" (ngSubmit)="submitForm()">
 
       <!-- Username section -->
+      <section class="username-section">
         <div class="form-floating mb-3">
           <input type="text" class="form-control" id="username" placeholder="username" formControlName="username" required>
           <label for="username">Username</label>
@@ -22,43 +23,51 @@ import { Component } from '@angular/core';
           </div>
 
         </div>
+      </section>
 
       <!-- Email section -->
-        <div class="mb-3">
-            <input type="email" class="form-control" id="email" placeholder="Email" formControlName="email" required>
-            <label for="email">Email</label>
-        </div>
-
-        <!-- Error handling for email attribute -->
-        <div *ngIf="email.invalid && (email.dirty || email.touched)" class="alert alert-danger">
-
-          <div *ngIf="email.errors?.['required']">
-            Email is required.
-          </div>  
-          <div *ngIf="email.errors?.['email']">
-            Email must be a valid email address.
+      <section class="email-section">
+          <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="email" placeholder="Email" formControlName="email" required>
+              <label for="email">Email</label>
           </div>
-        </div>
+  
+          <!-- Error handling for email attribute -->
+          <div *ngIf="email.invalid && (email.dirty || email.touched)" class="alert alert-danger">
+  
+            <div *ngIf="email.errors?.['required']">
+              Email is required.
+            </div>  
+            <div *ngIf="email.errors?.['email']">
+              Email must be a valid email address.
+            </div>
+          </div>
+      </section>
 
       <!-- Team section -->
+      <section class="team-section">
         <div class="form-floating mb-3">
-          <input class="form-control" type="text" formControlName="team" id="team" placeholder="Team" required>
-          <label for="team">Team</label>
-        </div>
 
-        <!-- Error handling for team  -->
-        <div *ngIf="team.invalid && (team.dirty || team.touched)" class="alert alert-danger">
-
-          <div *ngIf="team.errors?.['required']">
-            Team is required.
-          </div>
-          <div *ngIf="team.errors?.['required']">
-            Team must be one of the three: Instinct, Mystic, Valor.
+          <div class="form-check">
+            <input class="form-check-input" type="radio" formControlName="team" id="team-instinct" value="instinct" required>
+            <label class="form-check-label" for="team-instinct">Instinct</label>
           </div>
 
-        </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" formControlName="team" id="team-mystic" value="mystic" required>
+            <label class="form-check-label" for="team-mystic">Mystic</label>
+          </div>
 
-      <!-- Level section -->
+          <div class="form-check">
+            <input class="form-check-input" type="radio" formControlName="team" id="team-valor"Valor="valor"Valor>
+            <label class="form-check-label" for="team-valor">Valor</label>
+          </div>
+
+        </div>
+      </section>
+      <!-- End Sections -->
+    </form>
+
 
   `,
   styles: [
