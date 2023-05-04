6353e634ab4c633d1030ad160bef460feb9c48ce
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 16:50:03 -0700

    programming add-pogo-account-component

--- client/src/app/add-pogo-account/add-pogo-account.component.ts
+++ client/src/app/add-pogo-account/add-pogo-account.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { PogoAccounts } from 'src/pogo-accounts'
+import { PogoAccountsService } from 'src/pogo-accounts.service'
 
 @Component({
   selector: 'app-add-pogo-account',
   template: `
-    <p>
-      add-pogo-account works!
-    </p>
+    <h2 class="text-center m-5">Add Pogo Account</h2>
+    <app-pogo-accounts-form (submitted)="addPogoAccount($event)"></app-pogo-accounts-form>
   `,
   styles: [
   ]
 })
+
 export class AddPogoAccountComponent {
+  constructor(
+    private pogoAccountsService: PogoAccountsService,
+    private router: Router
+  ) { }
+
+  addPogoAccount(pogoAccount: PogoAccounts) {
+    this.pogoAccountsService.createPogoAccount(pogoAccount)
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl(['/pogo-accounts'])
+        },
+        error: (err) => {
+          alert("Failed to create POGO account")
+          console.log(err)
+        }
+    })
+  }
 
 }
