f849a60c9592f750b609dd4c0fce42b12b3d5013
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 17:14:31 -0700

    adding routes to app-routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PogoAccountsListComponent } from './pogo-accounts-list/pogo-accounts-list.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { PogoAccountsListComponent } from './pogo-accounts-list/pogo-accounts-list.component'
+import { AddPogoAccountComponent } from './add-pogo-account/add-pogo-account.component'
+import { EditPogoAccountComponent } from './edit-pogo-account/edit-pogo-account.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'pogo-accounts', pathMatch: 'full' },
   { path: 'pogo-accounts', component: PogoAccountsListComponent },
+  { path: 'pogo-accounts/add', component: AddPogoAccountComponent },
+  { path: 'pogo-accounts/edit/:id', component: EditPogoAccountComponent },
 ];
 
 @NgModule({
