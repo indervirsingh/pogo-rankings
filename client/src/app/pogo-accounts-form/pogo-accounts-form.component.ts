38b482eaf74e4d75518d222a20613466c312e572
Indervir Singh <iindervirsingh@gmail.com>
2023-05-04 17:16:58 -0700

    importing modules

--- client/src/app/pogo-accounts-form/pogo-accounts-form.component.ts
+++ client/src/app/pogo-accounts-form/pogo-accounts-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 
